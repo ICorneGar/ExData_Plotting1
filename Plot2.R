574d379fd674a82ab65d73d6b8bea8e0e04a386f
ICorneGar <raivanux@gmail.com>
2014-08-09 01:54:18 -0400

    Adding

---
+++ Plot2.R
@@ -0,0 +1,20 @@
+## Reading dataset
+raw_data <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+## Changing data from type character to type double (as date).
+raw_data$Date <- as.Date(raw_data$Date, format="%d/%m/%Y")
+
+## Creating the subset with the observations between 2007-02-01 and 2007-02-02
+sub_data <- subset(raw_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Converting Datetime  
+datetime <- paste(as.Date(sub_data$Date), sub_data$Time); sub_data$Datetime <- as.POSIXct(datetime)
+
+## Creating Plot 1 object
+plot(sub_data$Global_active_power~sub_data$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+## Saving to png file of 480x480 size
+dev.copy(png, file="plot2.png", height=480, width=480)
+
+## closing device
+dev.off()
