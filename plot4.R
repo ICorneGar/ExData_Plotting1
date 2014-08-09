3553c52b063e040081a8bf7a6f09f9687b9c9fc2
ICorneGar <raivanux@gmail.com>
2014-08-09 02:17:57 -0400

    Correcting plot 4

--- plot4.R
+++ plot4.R
@@ -14,12 +14,12 @@ datetime <- paste(as.Date(sub_data$Date), sub_data$Time); sub_data$Datetime <- a
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0)) #Plot layout
 
 with(sub_data, {
-                  plot(Global_active_power~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="") #plot (1,1)
-                  plot(Voltage~Datetime, type="l", ylab="Voltage (volt)", xlab="") #plot (1,2)
-                  plot(Sub_metering_1~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="") #plot (2,1)
+                  plot(Global_active_power~Datetime, type="l", ylab="Global Active Power", xlab="") #plot (1,1)
+                  plot(Voltage~Datetime, type="l", ylab="Voltage", xlab="") #plot (1,2)
+                  plot(Sub_metering_1~Datetime, type="l", ylab="Global Active Power", xlab="") #plot (2,1)
                   lines(Sub_metering_2~Datetime,col='Red'); lines(Sub_metering_3~Datetime,col='Blue')
                   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-                  plot(Global_reactive_power~Datetime, type="l", ylab="Global Rective Power (kilowatts)",xlab="") #plot (2,2)
+                  plot(Global_reactive_power~Datetime, type="l", ylab="Global_Rective_Power",xlab="") #plot (2,2)
                }
      )
 
